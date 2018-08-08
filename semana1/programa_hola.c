5bcf704da011fb74d0fe303631b841d6983f31b3
Garcia Valdez Daniel <garciavd2018@dci25-175.ugto.mx>
2018-08-08 09:26:01 -0500

    Cree mi primer programa y añadi cambios a un documento

---
+++ semana1/programa_hola.c
@@ -0,0 +1,11 @@
+    /*
+     Esto es un comentario. La terminal ignora lo que este entre estos simbolos y no es interpretado como codigo este programa imprime la típica frase “hola mundo a la pantalla”
+     Creado el 06/08/2018 por Daniel Garcia 
+    */
+#include<stdio.h>
+int main ()
+{
+printf("Hola Daniel :D\n");
+printf("Este es el primer programa del curso\n");
+}
+/*Fin del programa*/
