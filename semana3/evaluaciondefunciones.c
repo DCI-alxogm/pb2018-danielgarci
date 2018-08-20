f85a50a2f3877c1986bea7537ea31d7c56832c19
Garcia Valdez Daniel <garciavd2018@dci25-188.ugto.mx>
2018-08-20 13:47:11 -0500

    se actualizaron los ejercicios

--- semana3/evaluaciondefunciones.c
+++ semana3/evaluaciondefunciones.c
@@ -7,13 +7,7 @@ int main()
 
 
 {
-	float var;
-	float r;
-	int a;
-	int b;
-	int c;
-	int r2;
-	int r3;
+	float x,y,z,w,t;
 /*se declaran las variables, en este caso la variable r, es donde se asignara el valor de la conversion a radianes*/
 	printf("Este programa te calcula el valor de las funciones enlistadas para la variable X que tu introduces\n");
 	printf("y=exp(-x)\n");
@@ -21,12 +15,15 @@ int main()
 	printf("y=ln(x)+3x^2\n");
 	printf("El programa opera las funciones trigonometricas en radianes, por lo que ṕrimero hacer la conversion a radianes y despues evalua las funciones\n");
 	printf("Ingresa el valor a signar a la variable X\n");
-	scanf("%f",&var);
+	scanf("%f",&x);
 	/*se escanea el valor a asignar a la variable x */
-	r=var*((3.1416)/180);
-	a=exp(-r);
-	printf("y=exp(-x)= %f\n",a);
-	
+	t=x*((3.141159265)/180);
+	y=exp(-x);
+	printf("y=exp(-x)= %f\n",y);
+	z=cos(x)+(2*(tan(x/2)));
+	printf("y=cos(x)+2tan(x/2)= %f\n",z);
+	w=log(x)+(3*pow(x,2));
+	printf("y=ln(x)+3x^2= %f\n",w);
 	return 0;
 }
 
