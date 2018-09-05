e1cd1b1833e5f2436b92e1d70b4ea9efc164dff5
Garcia Valdez Daniel <garciavd2018@dci25-152.ugto.mx>
2018-09-05 09:59:32 -0500

    se modificaron ejercicios

--- semana5/ejercicio1.c
+++ semana5/ejercicio1.c
@@ -5,37 +5,40 @@ int main()
 
 {
 
-	int A;
-	int B;
-	float C;
-	float d;
+	int xi,xf,esp,i,n;
 	float exp_a,log_a,sen_a,cos_a,sqrt_a;
 
 
-	printf("Bienvenido, introduce el valor minimo que deseas evaluar\n");
-	scanf("%i",&d);
+	printf("Bienvenido, introduce el valor inicial que deseas evaluar\n");
+	scanf("%i",&xi);
 
-	printf("Introduce el valor maximo a evaluar\n");
+	printf("Introduce el valor final a evaluar\n");
 
-	scanf("%i",&B);
+	scanf("%i",&xf);
+	
+	
 
 	printf("Introduce el intervalo el espaciado entre cada salto\n");
 
-	scanf("%f",&C);
+	scanf("%f",&esp);
+	
 
-
-	for(A=d;A<B;A+C){
-		exp_a=exp(D);
-		printf("%f \t exp \n",D,exp_a);
-		log_a=log(D);
-		printf("%f \t log \n",D,log_a);
-		sen_a=sin(D);
-		printf("%f \t sen \n",D,sen_a);
-		cos_a=cos(D);
-		printf("%f \t cos \n",D,cos_a);
-		sqrt_a=sqrt(D);
-		printf("%f \t sqrt \n",D,sqrt_a);
+	n=(xf-xi)/esp;
+/*la n dice cuantas veces se va a realizar la operacion*/
+	for(i=0;i<n;i++){/*la i=0 dice que empieza en el cero de los valores del intervalo, no el cero como tal*/
+		exp_a=exp(xi);
+		printf("%f \t exp \n",xi,exp_a);
+		log_a=log(xi);
+		printf("%f \t log \n",xi,log_a);
+		sen_a=sin(xi);
+		printf("%f \t sen \n",xi,sen_a);
+		cos_a=cos(xi);
+		printf("%f \t cos \n",xi,cos_a);
+		sqrt_a=sqrt(xi);
+		printf("%f \t sqrt \n",xi,sqrt_a);
+		xi=xi+esp;/*esta operacion modifica a variable que se va a evaluar en las funciones, es independeiente del contador, pero debe ir acumulando el espaciado*/
 	}
+
 
 	return 0;
 }
