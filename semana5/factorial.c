879893a52b0c6f530e9a51483d7229109d1b553f
Daniel Garcia Valdez <danielgarciavaldez@localhost.localdomain>
2018-09-07 10:56:01 -0500

    actualice programas

---
+++ semana5/factorial.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+int main()
+
+{
+	int a;
+	int b;
+	int fact=1;
+	int op;
+
+	/*permite calcular el factorial de numero introducido por el usuario*/
+
+	
+	printf("Bievenido, presiona 1 para acceder\n");
+	scanf("%d",&op);
+		if(op==1){
+		
+		printf("Bienvenido, introduce el número del que deseas conocer el factorial\n");
+
+			scanf("%d",&a);
+
+				for(b=a;b>1;b--){
+					fact=fact*b;
+					printf("El factorial de %d = %d\n",a,fact);	
+						}
+		
+		}else{
+			printf("NOT DEFINED\n");
+		}
+			
+			return 0;
+}
+					
+					
