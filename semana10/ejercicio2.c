395d4cf8ec0d5bcb66c2ee21ad6e1a1209253681
Daniel Garcia Valdez <danielgarciavaldez@localhost.localdomain>
2018-10-17 09:11:57 -0500

    trabajos de la semana

---
+++ semana10/ejercicio2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void cuadrado();
+
+int main()
+
+{
+
+	cuadrado();
+	return 0;
+
+}
---
+++ semana10/ejercicio3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void cuadrado()
+
+{
+
+	float x;
+	float x2;
+	printf("Introduce un número\n");
+	scanf("%f",&x);
+	x2=x*x;
+	printf("El cuadrado del número %f es %f\n",x,x2);
+
+}
